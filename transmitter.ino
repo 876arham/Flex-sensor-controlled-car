6f95c4a61471a42d0107c5248a5f5baf28ee5b3e
876arham <35299150+876arham@users.noreply.github.com>
2019-11-15 11:43:45 +0530

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -5,12 +5,12 @@
 
 RH_ASK driver;                   //the library which helps us to control the radio modem
 
-char *msg = "000"; //Total number of data to be sent (data package)
+char *msg = "111"; //Total number of data to be sent (data package)
 
 //define the flex sensor input pins
 
-int flex_3 = A3;
-int flex_2 = A2;
+int flex_3 = A5;
+int flex_2 = A3;
 int flex_1 = A1;
 
 //define variables for flex sensor values
@@ -41,44 +41,36 @@ void loop(void){
 
  
   flex_3_val = analogRead(flex_3);
-  Serial.println(flex_3_val);
-  //pinky
-  if(flex_3_val>550)
-    msg[2]='1';
-  else{
-    msg[2] ='0';
-
-//thumb
+    flex_2_val = analogRead(flex_2);
     flex_1_val = analogRead(flex_1);
-  if(flex_1_val>550)
-   { msg[0]='1';
-    msg[1]='1';}
-  
-  else{
-
-    
-   flex_2_val = analogRead(flex_2);
-   //for left
-  if(flex_2_val>550)
-    {msg[0] = '0';
-    msg[1]='1';}
-    //for right
-  else
-    {msg[0] = '1';
-    msg[1]='0';}
-
- }
- }
-
+     flex_3_val=map(flex_3_val ,400,600,300,700);
+ // Serial.println(flex_3_val);
+  //index 
+  if(flex_1_val>620 && flex_2_val>550 && flex_3_val>600){
+    msg[2]='0';
+  }
+  else if(flex_1_val<=620 && flex_2_val<=550 && flex_3_val<=600){
+    msg="001";
+  }
+  else if(flex_1_val<=620 && flex_2_val<=550 && flex_3_val>600){
+    msg="011";
+  }
+  else if(flex_1_val>620 && flex_2_val<=550 && flex_3_val<=600){
+    msg="101";
+  }
+  else if(flex_1_val>620 && flex_2_val<=550 && flex_3_val>600){
+    msg="111";
+  }
     
 //msg[0] corresponds to left tyres movement
 //msg[1] corresponds to right tyres movement
-// msg[2] means stop 
+// msg[2] means motion
 // '0'value means backward rotation
 // '1' means forward rotation
 
 driver.send((uint8_t *)msg,strlen(msg));
 driver.waitPacketSent();
     delay(1);
+    Serial.println((char*)msg); 
  // radio.write(msg, sizeof(msg));
 }
